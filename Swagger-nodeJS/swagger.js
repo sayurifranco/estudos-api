7ac2fa659ec34a255c11808cbf7a0c18021c3bce
sayurifranco <lsshiroma@gmail.com>
2023-04-10 14:09:39 +0100

    Adição projeto com Swagger

---
+++ Swagger-nodeJS/swagger.js
@@ -0,0 +1,30 @@
+// No Swagger, existem ferramentas para os seguintes tipos de 
+// tarefas a serem realizadas para o completo desenvolvimento da 
+// API de um serviço WEB: 
+
+// Especificação da API, que consiste em determinar os modelos de 
+// dados que serão entendidos por ela e as funcionalidades presentes 
+// na mesma. Para cada funcionalidade, é preciso especificar o seu 
+// nome, os parâmetros que devem ser passados no momento de sua 
+// invocação e os valores que irão ser retornados aos usuários da API.
+
+// Entre esta ferramentas, podemos citar o OpenAPI Specification.
+// Após especificar a API, o framework facilita sua implementação. 
+// Com a ferramenta Swagger Codegen é possível montar o código 
+// inicial automaticamente nas principais linguagens de programação.
+
+// Os testes de API são extremamente importantes, pois ajudam a 
+// garantir o funcionamento, o desempenho e a confiabilidade da sua 
+// aplicação. O Swagger oferece ferramentas para teste manuais, 
+// automatizados e de desempenho 
+// Para auxiliar na utilização da API, o Swagger dispõe de ferramenta 
+// para deixar a visualização mais intuitiva, permitindo também que 
+// interajam com a API.
+// O Swagger permite criar a documentação da API de 3 formas:
+
+// Automaticamente: a documentação é gerada simultaneamente ao 
+// desenvolvimento da API.Manualmente: permite ao desenvolvedor 
+// escrever livremente as especificações da API e as publicar 
+// posteriormente em seu próprio servidor. Codegen: converte todas 
+// as anotações contidas no código fonte das APIs REST em documentação,
+// de forma global.
